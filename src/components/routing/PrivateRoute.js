0878f1acf53732b6fb52d23690bc73bd813795c6
riteshsoni123 <comedybuddy1@gmail.com>
2023-05-28 00:03:06 +0530

    made changes

--- src/components/routing/PrivateRoute.js
+++ src/components/routing/PrivateRoute.js
@@ -1,7 +1,11 @@
 import { Navigate } from "react-router-dom";
 
 const PrivateRoute = ({ children }) => {
-  return localStorage.getItem("authToken") ? children : <Navigate to="/game" />;
+  return localStorage.getItem("authToken") ? (
+    children
+  ) : (
+    <Navigate to="/register" />
+  );
 };
 
 export default PrivateRoute;
